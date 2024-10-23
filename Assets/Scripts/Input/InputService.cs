cf219c282f49fedc87202882c2ea3752b08e367c
nayan <nayankamalverma@gmail.com>
2024-10-23 16:31:20 +0530

    Created instances of Commands

--- Assets/Scripts/Input/InputService.cs
+++ Assets/Scripts/Input/InputService.cs
@@ -1,6 +1,6 @@
 using Command.Main;
 using Command.Player;
-using Command.Actions;
+using Command.Commands;
 
 namespace Command.Input
 {
@@ -9,7 +9,7 @@ namespace Command.Input
         private MouseInputHandler mouseInputHandler;
 
         private InputState currentState;
-        private ActionType selectedActionType;
+        private CommandType selectedCommandType;
         private TargetType targetType;
 
         public InputService()
@@ -29,11 +29,11 @@ namespace Command.Input
                 mouseInputHandler.HandleTargetSelection(targetType);
         }
 
-        public void OnActionSelected(ActionType selectedActionType)
+        public void OnActionSelected(CommandType selectedCommandType)
         {
-            this.selectedActionType = selectedActionType;
+            this.selectedCommandType = selectedCommandType;
             SetInputState(InputState.SELECTING_TARGET);
-            TargetType targetType = SetTargetType(selectedActionType);
+            TargetType targetType = SetTargetType(selectedCommandType);
             ShowTargetSelectionUI(targetType);
         }
 
@@ -43,12 +43,55 @@ namespace Command.Input
             GameService.Instance.UIService.ShowTargetOverlay(playerID, selectedTargetType);
         }
 
-        private TargetType SetTargetType(ActionType selectedActionType) => targetType = GameService.Instance.ActionService.GetTargetTypeForAction(selectedActionType);
+        private TargetType SetTargetType(CommandType selectedCommandType) => targetType = GameService.Instance.ActionService.GetTargetTypeForAction(selectedCommandType);
 
         public void OnTargetSelected(UnitController targetUnit)
         {
             SetInputState(InputState.EXECUTING_INPUT);
-            GameService.Instance.PlayerService.PerformAction(selectedActionType, targetUnit);
+
+            UnitCommand commandToProcess = CreateUnitCommand(targetUnit);
+
+            GameService.Instance.ProcessUnitCommand(commandToProcess);
+        }
+
+        private CommandData CreateCommandData(UnitController targetUnit)
+        {
+            // Create CommandData with the necessary information for a UnitCommand.
+            // It includes the ActiveUnit's ID, TargetUnit's ID, ActivePlayer's ID, and the TargetPlayer's ID.
+            return new CommandData(
+                GameService.Instance.PlayerService.ActiveUnitID,
+                targetUnit.UnitID,
+                GameService.Instance.PlayerService.ActivePlayerID,
+                targetUnit.Owner.PlayerID
+            );
+        }
+
+        private UnitCommand CreateUnitCommand(UnitController targetUnit)
+        {
+            // Create the necessary CommandData based on the target unit.
+            CommandData commandData = CreateCommandData(targetUnit);
+
+            // Based on the selected command type, create and return the corresponding UnitCommand.
+            switch (selectedCommandType)
+            {
+                case CommandType.Attack:
+                    return new AttackCommand(commandData);
+                case CommandType.Heal:
+                    return new HealCommand(commandData);
+                case CommandType.AttackStance:
+                    return new AttackStanceCommand(commandData);
+                case CommandType.Cleanse:
+                    return new CleanseCommand(commandData);
+                case CommandType.BerserkAttack:
+                    return new BerserkAttackCommand(commandData);
+                case CommandType.Meditate:
+                    return new MeditateCommand(commandData);
+                case CommandType.ThirdEye:
+                    return new ThirdEyeCommand(commandData);
+                default:
+                    // If the selectedCommandType is not recognized, throw an exception.
+                    throw new System.Exception($"No Command found of type: {selectedCommandType}");
+            }
         }
     }
 }
