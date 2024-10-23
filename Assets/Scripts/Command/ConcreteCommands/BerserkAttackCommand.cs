72693f034d59fa26eaaa71a907e775269dd0b0a5
nayan <nayankamalverma@gmail.com>
2024-10-23 16:08:05 +0530

    Created Concrete Commands

---
+++ Assets/Scripts/Command/ConcreteCommands/BerserkAttackCommand.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Command.Main;
+
+namespace Command.Commands
+{
+    public class BerserkAttackCommand : UnitCommand
+    {
+        private bool willHitTarget;
+        private const float hitChance = 0.66f;
+
+        public BerserkAttackCommand(CommandData commandData)
+        {
+            this.commandData = commandData;
+            willHitTarget = WillHitTarget();
+        }
+
+        public override bool WillHitTarget() => Random.Range(0f, 1f) < hitChance;
+
+        public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.BerserkAttack).PerformAction(actorUnit, targetUnit, willHitTarget);
+    }
+}
